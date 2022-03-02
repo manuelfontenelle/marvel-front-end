77829997ad122839e14701aff7ce60c4a6ed85a3
Manuel Fontenelle <manuel.fontenelle@gmail.com>
2022-03-02 23:51:41 +0100

    Favoris clean

--- src/pages/Favoris.js
+++ src/pages/Favoris.js
@@ -1,9 +1,9 @@
+import { Link } from "react-router-dom"
 import { useState, useEffect } from "react"
 import axios from "axios"
 import Loader from "react-js-loader"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faHeart } from "@fortawesome/free-solid-svg-icons"
-import { faHeart as faHeartRegular } from "@fortawesome/free-regular-svg-icons"
 
 const Favoris = () => {
 	const [data, setData] = useState([])
@@ -27,7 +27,7 @@ const Favoris = () => {
 				}
 				setData(newData)
 				// console.log(data)
-				// console.log("dataToken", dataToken)
+				console.log("dataToken", dataToken)
 				setIsLoading(false)
 			} catch (error) {
 				console.log(error.message)
@@ -37,26 +37,19 @@ const Favoris = () => {
 		fetchData()
 	}, [])
 
-	const setFav = (characterId) => {
-		console.log(characterId)
-
+	const setFav = (characterId, event) => {
+		// console.log(characterId)
 		const newdataToken = [...dataToken]
-
 		for (var i = 0; i <= newdataToken.length; i++) {
-			// console.log(newFavsId)
-			if (dataToken.indexOf(characterId) === -1) {
-				newdataToken.push(characterId)
-				setDataToken(newdataToken)
-				localStorage.setItem("favIdToken", JSON.stringify(newdataToken))
-				break
-			} else {
-				console.log("passage")
-				const characterIndex = dataToken.indexOf(characterId)
-				newdataToken.splice(characterIndex, 1)
-				setDataToken(newdataToken)
-				localStorage.setItem("favIdToken", JSON.stringify(newdataToken))
-				break
-			}
+			event.preventDefault()
+			const characterIndex = dataToken.indexOf(characterId)
+			newdataToken.splice(characterIndex, 1)
+			setDataToken(newdataToken)
+			localStorage.setItem("favIdToken", JSON.stringify(newdataToken))
+			const cardContainer =
+				event.target.parentNode.parentNode.parentNode.parentNode
+			cardContainer.style.display = "none"
+			break
 		}
 	}
 
@@ -83,46 +76,24 @@ const Favoris = () => {
 
 							return (
 								<>
-									<div className="card relative">
-										{dataToken.indexOf(item._id) === -1 ? (
-											<span
-												onClick={() => {
-													setFav(item._id)
-												}}
-												className="btn-favoris"
-											>
+									<Link to={`/character/${item._id}`}>
+										<div className="card relative" id="card">
+											<span className="btn-favoris active">
 												<FontAwesomeIcon
-													icon={faHeartRegular}
-													size="2x"
-													className="icone-fav"
-												/>
-											</span>
-										) : (
-											<span
-												onClick={() => {
-													setFav(item._id)
-												}}
-												className="btn-favoris active"
-											>
-												<FontAwesomeIcon
+													onClick={(event) => {
+														setFav(item._id, event)
+													}}
 													icon={faHeart}
 													size="2x"
 													className="icone-fav active"
 												/>
 											</span>
-										)}
-										<img className="picture" src={picture} alt="" />
-										<div className="card-content">
-											<p className="card-name">{item.name}</p>
-
-											{/* {character.description !== null &&
-									character.description !== "" ? (
-										<p className="card-desc">
-											{character.description.slice(0, 100) + "..."}
-										</p>
-									) : null} */}
+											<img className="picture" src={picture} alt="" />
+											<div className="card-content">
+												<p className="card-name">{item.name}</p>
+											</div>
 										</div>
-									</div>
+									</Link>
 								</>
 							)
 						})}
